b0bb668f5cb8247fd972712d9235fc3909473246
Aditya Sharma <adityashrm21@gmail.com>
2019-01-19 15:31:22 -0800

    added title and more comments to code

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+# loading required libraries
 library(tidyverse)
 library(shiny)
 
@@ -6,8 +7,11 @@ set.seed(100)
 function(input, output, session) {
   
   ## Interactive Map ###########################################
+  
+    # creating a reactive data object to be used in the plot
     dat <- reactive({
-      
+        
+        # filtering data bsed on user selected settings
         dat <- shootings %>% 
                 filter(race == input$race, armed == input$armed, 
                        gender == input$gender, flee == input$fleeing,
@@ -26,11 +30,14 @@ function(input, output, session) {
       projection = list(type = 'albers usa'),
       lakecolor = toRGB('white')
     )
+    # plot the map with the reactive dataset created based on user settings
     plot_ly(z = dat()$prop, text = dat()$state, locations = dat()$state,
-            type = 'choropleth', locationmode = 'USA-states') %>%
-      layout(geo = g)
+            type = 'choropleth', locationmode = 'USA-states', colorscale='Viridis') %>%
+            colorbar(title = '% of Total Shootings') %>% 
+            layout(geo = g, title = 'Poolice Shootings in the United States (2015 - Present)')
   })
   
+  # add some more interactivity in the future based on clicks
   # output$click <- renderPrint({
   #   d <- event_data("plotly_click")
   #   if (is.null(d)) "Click on a state to view event data" else d
@@ -44,36 +51,36 @@ function(input, output, session) {
     })
     
   # Precalculate the breaks we'll need for the two histograms
-     ageBreaks <- hist(plot = FALSE, shootings$age, breaks = 20)$breaks
+    ageBreaks <- hist(plot = FALSE, shootings$age, breaks = 20)$breaks
   
-  output$histAge <- renderPlot({
-    hist(shootings$age,
-         breaks = ageBreaks,
-         main = "Age of the person",
-         xlab = "Age",
-         xlim = lims(),
-         col = '#00DD00',
-         border = 'white')
+    output$histAge <- renderPlot({
+      hist(shootings$age,
+           breaks = ageBreaks,
+           main = "Age of the person",
+           xlab = "Age",
+           xlim = lims(),
+           col = '#00DD00',
+           border = 'white')
   })
   
   ## Data Explorer ###########################################
     
-  # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- shootings
-    if (input$gen != "All") {
-      data <- data[data$gender == input$gen,]
-    }
-    if (input$rac != "All") {
-      data <- data[data$race == input$rac,]
-    }
-    if (input$arm != "All") {
-      data <- data[data$armed == input$arm,]
-    }
-    if (input$flee != "All") {
-      data <- data[data$flee == input$flee,]
-    }
-    data
-  }))
+    # Filter data based on selections
+    output$table <- DT::renderDataTable(DT::datatable({
+      data <- shootings
+      if (input$gen != "All") {
+        data <- data[data$gender == input$gen,]
+      }
+      if (input$rac != "All") {
+        data <- data[data$race == input$rac,]
+      }
+      if (input$arm != "All") {
+        data <- data[data$armed == input$arm,]
+      }
+      if (input$flee != "All") {
+        data <- data[data$flee == input$flee,]
+      }
+      data
+    }))
   
 }
